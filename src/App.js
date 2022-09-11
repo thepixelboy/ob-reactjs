841015764248c7280e5b9c892482d0fceb5d47a0
Ruben <34570952+thepixelboy@users.noreply.github.com>
2022-09-11 11:58:36 +0200

    feat: create and organize new pages. install axios

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // import logo from "./logo.svg";
 import "./App.css";
-import ContactListComponent from "./components/containers/contact_list";
+// import ContactListComponent from "./components/containers/contact_list";
 // import Parent from "./components/containers/parent";
 // import { Clock } from "./hooks/lifecycle/Clock";
 // import GreetingStyled from "./components/pure/greetingStyled";
@@ -8,7 +8,7 @@ import ContactListComponent from "./components/containers/contact_list";
 // import MiComponenteConContexto from "./hooks/ejemplo3";
 // import Greeting from "./components/pure/greeting";
 // import GreetingF from "./components/pure/greetingF";
-// import TaskListComponent from "./components/containers/task_list";
+import TaskListComponent from "./components/containers/task_list";
 // import Contact from "./components/containers/contact";
 // import Ejemplo1 from "./hooks/ejemplo1";
 // import Ejemplo2 from "./hooks/ejemplo2";
@@ -34,8 +34,8 @@ function App() {
       {/* </header> */}
       {/* Gestión de eventos */}
       {/* <Parent></Parent> */}
-      {/* <TaskListComponent></TaskListComponent> */}
-      <ContactListComponent></ContactListComponent>
+      <TaskListComponent></TaskListComponent>
+      {/* <ContactListComponent></ContactListComponent> */}
     </div>
   );
 }
---
+++ src/pages/home/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Home page</h1>
+      <h2>Dashboard</h2>
+    </div>
+  );
+};
+
+export default HomePage;
